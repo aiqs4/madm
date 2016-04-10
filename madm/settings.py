4093ac8f8c306889228348733dfaf88f9d4ccf28
Sascha Dewald <sascha.dewald@gmail.com>
2016-04-10 18:07:45 +0200

    * Add django-gulp

--- madm/settings.py
+++ madm/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django_gulp',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
